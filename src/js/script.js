30215a5f54345e7d0645118590f4d55c8dbcd48f
thi_nguyen <thi.nguyen@ksb-sg.ch>
2022-11-15 12:00:52 +0100

    Elden Ring :(

--- src/js/script.js
+++ src/js/script.js
@@ -14,29 +14,30 @@ searchInput.addEventListener("input", (e) => {
         item.name.toLowerCase().includes(value) || 
         item.type.toLowerCase().includes(value)
         item.itemelement.classList.toggle("hide", !itemisVisible)
-    })
-})
-searchInput.addEventListener("input", (e) => {
-    const value = e.target.value.toLowerCase()
+    }) 
     ammos.forEach(ammo => {
+        if (ammo.type !== null ) {
         const ammoisVisible = 
         ammo.name.toLowerCase().includes(value) || 
         ammo.type.toLowerCase().includes(value)
         ammo.ammoelement.classList.toggle("hide", !ammoisVisible)
-    })
+}})
 })
+
+
+
 
 
 fetch('https://eldenring.fanapis.com/api/items?limit=100').then(res => res.json()) //res = response
 .then(data => {
    items = data.data.map(item => { 
-    console.log(data);
     const itemcard = itemCardTemplate.content.cloneNode(true).children[0] //forEach ist eine Funktion, die nur mit Maps oder Arrays funktioniert
     const itemheader = itemcard.querySelector("[data-item-header]")
     const itembody = itemcard.querySelector("[data-item-body]")
     itemheader.textContent = item.name
     itembody.textContent = item.type
     itemCardContainer.append(itemcard)
+    console.log(itemcard);
     return { name: item.name, type: item.type, itemelement: itemcard}
     })  
 })
@@ -44,14 +45,15 @@ fetch('https://eldenring.fanapis.com/api/items?limit=100').then(res => res.json(
 
 fetch('https://eldenring.fanapis.com/api/ammos?limit=100').then(res => res.json())
 .then(data => {
-    console.log(data)
     ammos = data.data.map(ammo => {
+        console.log(data)
         const ammocard = ammoCardTemplate.content.cloneNode(true).children[0]
         const ammoheader = ammocard.querySelector("[data-ammo-header]")
         const ammobody = ammocard.querySelector("[data-ammo-body]")
         ammoheader.textContent = ammo.name
         ammobody.textContent = ammo.type
         ammoCardContainer.append(ammocard)
+        console.log(ammocard)
         return { name: ammo.name, type: ammo.type, ammoelement: ammocard}
         })    
 })
