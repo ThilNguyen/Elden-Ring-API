18ee76f4af62205b19abf39827be6620da7aa991
thi_nguyen <thi.nguyen@ksb-sg.ch>
2022-11-15 09:42:40 +0100

    Elden Ring-API

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,57 @@
 const itemCardTemplate = document.querySelector("[data-item-template]")
 const itemCardContainer = document.querySelector("[data-item-cards-container]")
+const ammoCardContainer = document.querySelector("[data-ammo-cards-container]")
+const ammoCardTemplate = document.querySelector("[data-ammo-template]")
 const searchInput = document.querySelector("[data-search]")
 
 let items = []
+let ammos = []
 
 searchInput.addEventListener("input", (e) => {
     const value = e.target.value.toLowerCase()
     items.forEach(item => {
-        const isVisible = 
+        const itemisVisible = 
         item.name.toLowerCase().includes(value) || 
         item.type.toLowerCase().includes(value)
-        item.element.classList.toggle("hide", !isVisible)
+        item.itemelement.classList.toggle("hide", !itemisVisible)
+    })
+})
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase()
+    ammos.forEach(ammo => {
+        const ammoisVisible = 
+        ammo.name.toLowerCase().includes(value) || 
+        ammo.type.toLowerCase().includes(value)
+        ammo.ammoelement.classList.toggle("hide", !ammoisVisible)
     })
 })
 
-fetch('https://eldenring.fanapis.com/api/items')
-.then(res => res.json()) //res = response
+
+fetch('https://eldenring.fanapis.com/api/items?limit=100').then(res => res.json()) //res = response
 .then(data => {
    items = data.data.map(item => { 
-    const card = itemCardTemplate.content.cloneNode(true).children[0] //forEach ist eine Funktion, die nur mit Maps oder Arrays funktioniert
-    const header = card.querySelector("[data-header]")
-    const body = card.querySelector("[data-body]")
-    header.textContent = item.name
-    body.textContent = item.type
-    itemCardContainer.append(card)
-    return { name: item.name, type: item.type, element: card}
-    })
+    console.log(data);
+    const itemcard = itemCardTemplate.content.cloneNode(true).children[0] //forEach ist eine Funktion, die nur mit Maps oder Arrays funktioniert
+    const itemheader = itemcard.querySelector("[data-item-header]")
+    const itembody = itemcard.querySelector("[data-item-body]")
+    itemheader.textContent = item.name
+    itembody.textContent = item.type
+    itemCardContainer.append(itemcard)
+    return { name: item.name, type: item.type, itemelement: itemcard}
+    })  
+})
+
+
+fetch('https://eldenring.fanapis.com/api/ammos?limit=100').then(res => res.json())
+.then(data => {
+    console.log(data)
+    ammos = data.data.map(ammo => {
+        const ammocard = ammoCardTemplate.content.cloneNode(true).children[0]
+        const ammoheader = ammocard.querySelector("[data-ammo-header]")
+        const ammobody = ammocard.querySelector("[data-ammo-body]")
+        ammoheader.textContent = ammo.name
+        ammobody.textContent = ammo.type
+        ammoCardContainer.append(ammocard)
+        return { name: ammo.name, type: ammo.type, ammoelement: ammocard}
+        })    
 })
